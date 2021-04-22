4832429218a27650071cae4f4290869814210a14
a.grigorev <a.grigorev@samsung.com>
2021-04-22 22:55:19 +0300

    readme added

--- docker/run.sh
+++ docker/run.sh
@@ -3,7 +3,5 @@
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source ${CURRENT_DIR}/source.sh
 
-docker pull $HEAD_NAME
 CODE_DIR=/mounted/${CURRENT_DIR}/..
-# NV_GPU=$(nvidia-smi --query-gpu=uuid --format=csv,noheader | tr '\n' ',') nvidia-docker run -ti $PARAMS $VOLUMES $NAME $@
 NV_GPU=$(nvidia-smi --query-gpu=uuid --format=csv,noheader | tr '\n' ',') nvidia-docker run -w=${CODE_DIR} -ti $PARAMS $VOLUMES $HEAD_NAME $@
--- docker/source.sh
+++ docker/source.sh
@@ -3,5 +3,4 @@ PORT=8086
 
 PARAMS="-p ${PORT}:${PORT} --net=host --ipc=host -u $(id -u ${USER}):$(id -g ${USER})"
 NAME="neural_textures"
-HEAD_NAME="airuhead01:5000/${NAME}"
 VOLUMES="-v /:/mounted"
